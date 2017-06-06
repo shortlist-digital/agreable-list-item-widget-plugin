4f8adb5442154d2ab1fdd5650e2dc8607b8b4210
Leo <leonoragilmour@sky.com>
2017-06-06 17:16:34 +0100

    init

---
+++ agreable-list-item-widget-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: Agreable List Item Widget Plugin
+* Plugin URI: http://github.com/shortlist-digital/agreable-list-item-widget-plugin
+* Description: A plugin for the list item widget
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class AgreableListItemWidgetPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new AgreableListItemWidgetPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Agreable List Item Widget Plugin
+A plugin for the list item widget
